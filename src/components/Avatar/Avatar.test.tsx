ba191d8c3d9bc4197e6f46bb5d5a1b4f3feec539
Oyindamola Oyetunmibi <oyindadavid18@gmail.com>
2023-06-12 14:25:37 +0100

    implement a fallback avatar url

--- src/components/Avatar/Avatar.test.tsx
+++ src/components/Avatar/Avatar.test.tsx
@@ -1,13 +1,25 @@
 import { describe, expect, it } from "vitest";
 import { render, screen } from "@testing-library/react";
 
-import Avatar from "./Avatar";
+import Avatar, {
+  FALLBACK_AVATAR_ALT_TEXT,
+  FALLBACK_AVATAR_URL,
+} from "./Avatar";
 
 describe("Avatar", () => {
-  it("should render an img with alt text", () => {
+  it("should render the url and alt passed in", () => {
+    const url = "https://cataas.com/cat/says/hello%20world!";
+    const alt = "a beautiful cat";
+    render(<Avatar url={url} alt={alt} />);
+
+    const img = screen.getByAltText(alt);
+    expect(img).toHaveAttribute("src", url);
+  });
+
+  it("should use a fallback image if no URL passed in", () => {
     render(<Avatar />);
-    const img = screen.getByAltText("@github-handle");
-    expect(img).toBeInTheDocument();
-    expect(img.tagName).toBe("IMG");
+
+    const img = screen.getByAltText(FALLBACK_AVATAR_ALT_TEXT);
+    expect(img).toHaveAttribute("src", FALLBACK_AVATAR_URL);
   });
 });
--- src/components/Avatar/Avatar.tsx
+++ src/components/Avatar/Avatar.tsx
@@ -1,10 +1,19 @@
-function Avatar() {
-  return (
-    <img
-      src="https://cataas.com/cat/says/hello%20world!"
-      alt="@github-handle"
-    />
-  );
+export const FALLBACK_AVATAR_ALT_TEXT = "@hello-cat";
+export const FALLBACK_AVATAR_URL =
+  '"https://cataas.com/cat/says/hello%20world!";';
+
+type AvatarProps = {
+  /** the url of an image to render for the Avatar. **/
+  url?: string;
+  /** the alt text to use for the url image. **/
+  alt?: string;
+};
+
+function Avatar({
+  url = FALLBACK_AVATAR_URL,
+  alt = FALLBACK_AVATAR_ALT_TEXT,
+}: AvatarProps) {
+  return <img src={url} alt={alt} />;
 }
 
 export default Avatar;
